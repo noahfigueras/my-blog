f306c5b09a8877dd329f773a5f417b7d0590f80c
noah figueras <noahfigueras@gmail.com>
2022-02-20 20:14:04 -0800

    Added: Display of dynamic posts in home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
 
+const Posts = ({list}) => {
+	
+	const posts = [];
+
+	for (let post of list) {
+		posts.push(
+			<div className="single-post">
+				<span className="post-date text-accent">{post.date}</span>	
+				<h3><a href={post.title}>{post.title}</a></h3>
+			</div>
+		)
+	}
+	return posts;
+}
+
 const App = () => {
+	const meta_posts = [
+		{
+			title: "All you need to know to pass the Comptia Security +",
+			date: "02/20/2022"
+		},
+		{
+			title: "Backpackig 4 days through the island of Maui - Hawaii",
+			date: "02/18/2022"
+		}
+	];
 	return (
 	<div className="container">
-		<h1> Noah Figueras's Website </h1>
-		<hr className="text-accent"/>
-		<section id="blog" className="grid">
-			<div className="post uppercase">Post</div>
-			<div className="post">Post</div>
-			<div className="post">Post</div>
-		</section>
+			<h1 className="center"> Noah Figueras's Website </h1>
+			<hr/>
+			<div className="all-posts grid">
+				<Posts list={meta_posts}/>	
+			</div>
 	</div>
 	);
 }
