cb2e9a7c2f86d936a033933fda40e84dc2d076ec
noah figueras <noahfigueras@gmail.com>
2022-03-15 18:19:47 -0700

    ADD: Fetch title of blog from file at home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useParams } from "react-router-dom";
 import * as runtime from 'react/jsx-runtime.js'
-import Example from './blog/example.mdx'
 import {evaluate} from '@mdx-js/mdx'
 
 const Posts = ({list}) => {
-	
-	const posts = [];
+	const [posts, setPosts] = useState([]);
 
-	for (let post of list) {
-		posts.push(
-			<div className="single-post">
-				<span className="post-date text-accent">{post.date}</span>	
-				<h3><Link to={post.title}>{post.title}</Link></h3>
-			</div>
-		)
+	const getDate = (name) => {
+		const i = name.indexOf("--");
+		return name.slice(i+2).replace(".mdx", "");
 	}
-	return posts;
+	
+	const getTitle = async (name) => {
+		try {
+			const response = await fetch(`https://raw.githubusercontent.com/noahfigueras/my-blog/master/blog/${name}`);
+			const data = await response.text();
+			return data.slice(0,data.indexOf("  ")).replace("# ", "");
+		} catch(err) {
+			console.log(err)
+		}
+	}
+
+	useEffect(() => {
+		const getPosts = async () => {
+			try{
+				for (let post of list) {
+					const date = getDate(post.name);
+					const title = await getTitle(post.name);
+					const element = (
+						<div className="single-post">
+							<span className="post-date text-accent">{date}</span>	
+							<h3><Link to={post.name}>{title}</Link></h3>
+						</div>
+						);
+					setPosts(oldArray => [...oldArray, element]);
+				}
+			} catch(err) {
+				console.log(err)
+			}
+		}
+		getPosts()
+	},[])
+	return posts.length < 1 ? null : posts;
 }
 
 const Post = () => {
@@ -26,7 +51,7 @@ const Post = () => {
 	useEffect(() => {
 		// Get Post
 		const post = async () => {
-			const response = await fetch("https://raw.githubusercontent.com/noahfigueras/my-blog/master/src/components/example.mdx");
+			const response = await fetch(`https://raw.githubusercontent.com/noahfigueras/my-blog/master/blog/${params.postId}`);
 			const data = await response.text();
 			setContent(await evaluate(data, {...runtime}));
 		}
@@ -37,22 +62,25 @@ const Post = () => {
 }
 
 const Home = () => {
-	const meta_posts = [
-		{
-			title: "post1",
-			date: "02/20/2022"
-		},
-		{
-			title: "post2",
-			date: "02/18/2022"
+	const [posts, setPosts] = useState(null);
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const response = await fetch("https://api.github.com/repos/noahfigueras/my-blog/contents/blog");
+			const data = await response.json();
+			setPosts(data);	
 		}
-	];
+		fetchPosts();
+	}, []);
+
 	return (
 	<div className="container">
 			<h1 className="center"> Noah Figueras's Website </h1>
 			<hr/>
 			<div className="all-posts grid">
-				<Posts list={meta_posts}/>	
+				{posts != null &&
+					<Posts list={posts}/>	
+				}
 			</div>
 	</div>
 	);
